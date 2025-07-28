617b7727a3d82911e2942c34b0b1202e6b7777ae
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-07-28 22:56:54 +0530

    Updated the MongoFile

--- utils/upload.js
+++ utils/upload.js
@@ -1,26 +1,19 @@
-const AWS = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
+const { S3Client } = require("@aws-sdk/client-s3");
+const multer = require("multer");
+const multerS3 = require("multer-s3-v3");
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  region: process.env.AWS_REGION,
-});
+const s3 = new S3Client({ region: process.env.AWS_REGION });
 
-const s3 = new AWS.S3();
-
-export const upload = multer({
+const upload = multer({
   storage: multerS3({
     s3,
     bucket: process.env.AWS_S3_BUCKET_NAME,
-    acl: 'public-read',
-    metadata: (req, file, cb) => {
-      cb(null, { fieldName: file.fieldname });
-    },
+    acl: "public-read",
     key: (req, file, cb) => {
       const uniqueName = `${Date.now().toString()}-${file.originalname}`;
       cb(null, `user-docs/${uniqueName}`);
-    }
+    },
   }),
 });
+
+module.exports = upload;
